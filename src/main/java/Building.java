4b6289d27d9aa9c7ebc45d80c3f178118b30f929
Connor Shillady <c.shillady24@gmail.com>
2022-05-04 10:03:53 -0400

    HW update

--- src/main/java/Building.java
+++ src/main/java/Building.java
@@ -1,12 +1,29 @@
 public class Building {
     // Variable Declarations
-    public double buildingWidth;
-    public double buildingLength;
-    public int buildingPrice;
+    private double buildingWidth;
+    private double buildingLength;
+    private int buildingPrice;
     public String buildingColor;
     // Constructors
     public Building () {}
+    public Building (double buildingWidth, double buildingLength, int buildingPrice) {
+        this.buildingWidth = buildingWidth;
+        this.buildingLength = buildingLength;
+        this.buildingPrice = buildingPrice;
+    }
     // Methods
     // Accessors
+    public double getBuildingWidth() {
+        return buildingWidth;
+    }
+    public double getBuildingLength() {
+        return buildingLength;
+    }
+    public int getBuildingPrice() {
+        return buildingPrice;
+    }
+    public String getBuildingColor() {
+        return buildingColor;
+    }
     // Overrides
 }
--- src/main/java/House.java
+++ src/main/java/House.java
@@ -1,31 +1,36 @@
+import java.util.ArrayList;
+
 public class House extends Building {
     // Variable Declarations
+    private ArrayList<Room> rooms;
     // Constructors
     public House () {}
     public House (double buildingWidth, double buildingLength, int buildingPrice, String buildingColor) {
-        this.buildingWidth = buildingWidth;
-        this.buildingLength = buildingLength;
-        this.buildingPrice = buildingPrice;
+        super(buildingWidth, buildingLength, buildingPrice);
         this.buildingColor = buildingColor;
+        this.rooms = new ArrayList<Room>();
 
-        var room1 = new Room("Kitchen", 15, 15, 8, 2);
-        var room2 = new Room("Living Room", 15, 15, 8, 4);
-        var room3 = new Room("Bathroom", 5, 5, 8, 1);
-        var room4 = new Room("Master Bedroom", 15, 10, 8, 3);
-        var room5 = new Room("Bedroom", 10, 10, 8, 2);
-
-        System.out.println(room1);
-        System.out.println(room2);
-        System.out.println(room3);
-        System.out.println(room4);
-        System.out.println(room5);
+//        var room1 = new Room("Kitchen", 15, 15, 8, 2);
+//        var room2 = new Room("Living Room", 15, 15, 8, 4);
+//        var room3 = new Room("Bathroom", 5, 5, 8, 1);
+//        var room4 = new Room("Master Bedroom", 15, 10, 8, 3);
+//        var room5 = new Room("Bedroom", 10, 10, 8, 2);
+//
+//        System.out.println(room1);
+//        System.out.println(room2);
+//        System.out.println(room3);
+//        System.out.println(room4);
+//        System.out.println(room5);
     }
     // Methods
+    public void addRoom(Room room) {
+        rooms.add(room);
+    }
     // Accessors
     // Overrides
     @Override
     public String toString() {
-        String roomDescription = buildingWidth + "ft  " + buildingLength + "ft  $" + buildingPrice + "  " + buildingColor;
+        String roomDescription = getBuildingWidth() + "ft  " + getBuildingLength() + "ft  $" + getBuildingPrice() + "  " + buildingColor;
         return roomDescription;
     }
 }
